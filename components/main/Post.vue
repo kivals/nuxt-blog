6ac73965ddedc8326a9b04bbcd578d6d4cfa9417
melnik <melnik@bifit.com>
2019-12-28 10:35:11 +0300

    add Post's component

---
+++ components/main/Post.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-card
+    shadow="hover"
+    body-style="{paddind: 0}"
+    class="post"
+  >
+  <header slot="header" class="post-header">
+    <h3>Post title</h3>
+    <small>
+      <i class="el-icon-time"></i>
+      {{ new Date().toLocaleString() }}
+      </small>
+  </header>
+  <div class="post-body">
+    <img 
+      src="https://cdn.anadventurousworld.com/wp-content/uploads/2019/01/where-to-stay-in-prague.jpg" 
+      alt="post-img" 
+      class="post-img">
+  </div>
+  <footer class="post-footer">
+    <el-button round @click="openPost">Открыть</el-button>
+    <span>
+      <i class="el-icon-message">
+        12
+      </i>
+    </span>
+  </footer>
+  </el-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPost() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+  .post {
+    margin-bottom: 1.5rem;
+  }
+  .post-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .post-img{
+    width: 100%;
+    height: auto;
+  }
+
+  .post-footer{
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :ms="12" :lg="10">
+      <AppPost
+        v-for="post in 3"
+        :key="post">
+
+      </AppPost>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-
+import AppPost from '@/components/main/Post'
 export default {
+  head: {
+    title: 'Главная страница'
+  },
+  components: {
+    AppPost
+  }
 }
 </script>
